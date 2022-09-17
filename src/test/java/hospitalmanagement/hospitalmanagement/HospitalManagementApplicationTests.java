4f113c0d0cf4440a8239aabf754a00ef182e8573
shubham-saini18 <110394904+shubham-saini18@users.noreply.github.com>
2022-09-17 11:35:30 +0000

    Update HospitalManagementApplicationTests.java

--- src/test/java/hospitalmanagement/hospitalmanagement/HospitalManagementApplicationTests.java
+++ src/test/java/hospitalmanagement/hospitalmanagement/HospitalManagementApplicationTests.java
@@ -1,13 +1,1 @@
-package hospitalmanagement.hospitalmanagement;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HospitalManagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
